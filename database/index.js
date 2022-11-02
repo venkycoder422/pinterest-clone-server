4e144af6eaba2d4dd88123a650b623f56f44eac0
venky422 <dommaravenkatesulu22@gmail.com>
2022-11-02 22:52:21 +0530

    server added and deploying

--- database/index.js
+++ database/index.js
@@ -1,9 +1,17 @@
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const mongoose = require('mongoose')
 async function connectDB() {
     try {
         // const url = 'mongodb://0.0.0.0:27017/pinterestClone';
-        const url = "mongodb+srv://VenkatesuluD:289919@cluster0.ju6pfuk.mongodb.net/?retryWrites=true&w=majority"
-        mongoose.connect(url);
+        const url = process.env.MONGO_URI
+        mongoose.connect(url,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        });
         console.log('connected successfully');
     } catch (error) {
         console.log('connection not done');
