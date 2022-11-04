94c1a509376fd51b6efd8744e40ef76366bfdc03
venky422 <dommaravenkatesulu22@gmail.com>
2022-11-05 00:11:17 +0530

    index js changed

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+// app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build'));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/build'));
     
-})
+// })
 
-app.get('/h', (req, res) => {
+app.get('/', (req, res) => {
     res.send("API Running")
 })
 
